5ea2ee4a3cb09870b7f7c74b4686d3f948973d3f
Julien-Baiwir <julien.freddy.baiwir@gmail.com>
2024-01-11 15:56:50 +0100

    premier point

--- README.md
+++
@@ -1,1 +0,0 @@
-# random-quote-using-async-await
--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+// https://api.dictionaryapi.dev/api/v2/entries/en/hello
+// https://api.dictionaryapi.dev/api/v2/entries/en/bye possible d'entrer le lien direct
 
+const fetchWordDefinition = (word) => fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
 
+document.getElementById("addBtn").addEventListener("click", function() {
+    const InputWord = document.getElementById("wordInput").value;
+    fetchWordDefinition(InputWord)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then((json) => {
+            
+            console.log(json);
+        })
+        .catch((error) => {
+            console.error("There was an error!", error);
+        });
+});
 
